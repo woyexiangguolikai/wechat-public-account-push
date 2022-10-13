66fdd451f39692e0223438a53c081c59cb488805
woyexiangguolikai <115533176+woyexiangguolikai@users.noreply.github.com>
2022-10-13 19:37:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -32,16 +32,13 @@ const USER_CONFIG = {
         {
           type: '生日', name: '小叶', year: '2002', date: '03-16',
         },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
-        // 在一起的日子
+        // 认识的日子
         { keyword: 'love_day', date: '2022-07-23' },
-        // 结婚纪念日
-
+        // 在一起的日子
+        { keyword: 'marry_day', date: '2022-08-27' },
       ],
     },
   ],
