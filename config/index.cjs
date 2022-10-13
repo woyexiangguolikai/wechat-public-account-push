a888878965391947e948a245a82f46e0ec719635
woyexiangguolikai <115533176+woyexiangguolikai@users.noreply.github.com>
2022-10-13 19:45:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小叶',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oRJTa6YUgY5yBf-qpgKb8o5jOOxA',
+      id: 'oRJTa6bkJsICFFm3L4qolwmURK1Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'BI7WVaa_l4g8Q3GMGAN0SQSDPuvpZcp5LLkkN6LnpjE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
